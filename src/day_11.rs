f511677435e06b34ee57c79201e7f7c7a4909135
mrtolkien <gary.mialaret@gmail.com>
2022-12-11 23:16:18 +0900

    Working monkey parsing (except operation)

--- src/day_11.rs
+++ src/day_11.rs
@@ -1,6 +1,13 @@
-use nom::{bytes::complete::tag, character::complete::digit1, sequence::delimited, IResult};
+use nom::{
+    bytes::complete::{is_not, tag},
+    character::complete::digit1,
+    combinator::map_res,
+    sequence::{delimited, preceded, tuple},
+    IResult,
+};
 
 struct Monkey {
+    id: usize,
     starting_items: Vec<usize>,
     operation: Box<dyn Fn(usize) -> usize>,
     divisible_by: usize,
@@ -12,31 +19,118 @@ pub fn get_monkey_business_level(input: &str) -> usize {
     todo!()
 }
 
-fn parse_monkey(s: &str) -> IResult<&str, Monkey> {
-    let (_, monkey_id) = delimited(tag("Monkey "), digit1, tag(":"))(s)?;
-    let (_, starting_items) = delimited(tag("Starting items: "), digit1, tag("\n"))(s)?;
-    let (_, operation_str) = delimited(tag("Operation: "), digit1, tag("\n"))(s)?;
-    let (_, divisible_by) = delimited(tag("Test: divisible by "), digit1, tag("\n"))(s)?;
+fn parse_monkey(input: &str) -> IResult<&str, Monkey> {
+    let (_, (monkey_id, items, operation, divisible_by, true_target, false_target)) = tuple((
+        nom_monkey,
+        nom_items,
+        nom_operation,
+        nom_divisible_by,
+        nom_throw_to_true,
+        nom_throw_to_false,
+    ))(input)?;
 
-    todo!();
+    Ok((
+        "",
+        Monkey {
+            id: monkey_id,
+            starting_items: items,
+            operation: Box::new(|x| x),
+            divisible_by,
+            if_true: true_target,
+            if_false: false_target,
+        },
+    ))
+}
+
+fn nom_monkey(s: &str) -> IResult<&str, usize> {
+    map_res(
+        delimited(tag("Monkey "), digit1, tag(":\n")),
+        |out: &str| usize::from_str_radix(out, 10),
+    )(s)
+}
+
+fn nom_items(s: &str) -> IResult<&str, Vec<usize>> {
+    let (remainder, items) = delimited(tag("Starting items: "), is_not("\n"), tag("\n"))(s)?;
+    // TODO Understand that one
+    // let (_, items) = separated_list0(tag(", "), is_not("/"))(items)?;
+
+    // In the meanwhile, this works
+    Ok((
+        remainder,
+        items.split(", ").map(|s| s.parse().unwrap()).collect(),
+    ))
+}
+
+fn nom_operation(s: &str) -> IResult<&str, &str> {
+    delimited(tag("Operation: new = "), is_not("\n"), tag("\n"))(s)
+}
+
+fn nom_divisible_by(s: &str) -> IResult<&str, usize> {
+    map_res(
+        delimited(tag("Test: divisible by "), digit1, tag("\n")),
+        |out: &str| usize::from_str_radix(out, 10),
+    )(s)
+}
+
+fn nom_throw_to_true(s: &str) -> IResult<&str, usize> {
+    map_res(
+        delimited(tag("    If true: throw to monkey "), digit1, tag("\n")),
+        |out: &str| usize::from_str_radix(out, 10),
+    )(s)
+}
+
+fn nom_throw_to_false(s: &str) -> IResult<&str, usize> {
+    map_res(
+        preceded(tag("    If false: throw to monkey "), digit1),
+        |out: &str| usize::from_str_radix(out, 10),
+    )(s)
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
-    #[test]
-    fn test_parse_monkey() {
-        let monkey = parse_monkey(
-            "Monkey 0:
+    const DEMO_MONKEY: &str = "Monkey 0:
 Starting items: 79, 98
 Operation: new = old * 19
 Test: divisible by 23
-If true: throw to monkey 2
-If false: throw to monkey 3",
-        )
-        .unwrap()
-        .1;
+    If true: throw to monkey 2
+    If false: throw to monkey 3";
+
+    #[test]
+    fn test_nom() {
+        // Just testing nom for my sanity
+        let (_, res) = nom_monkey(DEMO_MONKEY).unwrap();
+        assert_eq!(res, 0);
+
+        let (_, (_, res)) = tuple((nom_monkey, nom_items))(DEMO_MONKEY).unwrap();
+        assert_eq!(res, vec![79, 98]);
+
+        let (_, (_, _, res)) = tuple((nom_monkey, nom_items, nom_operation))(DEMO_MONKEY).unwrap();
+        assert_eq!(res, "old * 19");
+
+        let (_, (_, _, _, res)) =
+            tuple((nom_monkey, nom_items, nom_operation, nom_divisible_by))(DEMO_MONKEY).unwrap();
+        assert_eq!(res, 23);
+
+        let (_, (_, _, _, _, true_target, false_target)) = tuple((
+            nom_monkey,
+            nom_items,
+            nom_operation,
+            nom_divisible_by,
+            nom_throw_to_true,
+            nom_throw_to_false,
+        ))(DEMO_MONKEY)
+        .unwrap();
+
+        assert_eq!(true_target, 2);
+        assert_eq!(false_target, 3);
+    }
+
+    #[test]
+    fn test_parse_monkey() {
+        let monkey = parse_monkey(DEMO_MONKEY).unwrap().1;
 
         assert_eq!(monkey.starting_items, vec![79, 98]);
         assert_eq!(monkey.divisible_by, 23);
