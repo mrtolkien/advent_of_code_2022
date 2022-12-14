5deb2951da6d4636a1dd2f93133cd15efdf7ed4a
mrtolkien <gary.mialaret@gmail.com>
2022-12-14 15:00:05 +0900

    Reworked -> Everything is a Value

--- src/day_13.rs
+++ src/day_13.rs
@@ -1,5 +1,3 @@
-use std::iter::zip;
-
 use nom::multi::separated_list0;
 use nom::sequence::separated_pair;
 use nom::{
@@ -12,17 +10,7 @@ pub fn get_right_order_pairs_index_sum(input: &str) -> usize {
     let mut result = 0;
 
     for (idx, (left, right)) in input.iter().enumerate() {
-        // TODO Check behaviour when one side is longer than the other
-        for (left_value, right_value) in zip(left, right) {
-            match (left_value, right_value) {
-                (Value::Number(_), Value::Number(_)) => todo!(),
-                (Value::Array(_), Value::Array(_)) => todo!(),
-                (Value::Number(_), Value::Array(_)) => todo!(),
-                (Value::Array(_), Value::Number(_)) => todo!(),
-            }
-        }
-        // TODO Get the right test here!
-        if left == right {
+        if left <= right {
             result += idx + 1;
         }
     }
@@ -30,21 +18,23 @@ pub fn get_right_order_pairs_index_sum(input: &str) -> usize {
     result
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, PartialOrd)]
 enum Value {
     Number(usize),
     Array(Vec<Value>),
 }
 
-fn parse_input(input: &str) -> Vec<(Vec<Value>, Vec<Value>)> {
+// TODO !ORDERING CODE!
+
+fn parse_input(input: &str) -> Vec<(Value, Value)> {
     match separated_list0(tag("\n\n"), parse_packet)(input) {
         Ok((_, values)) => values,
         Err(_) => panic!("Failed to parse input: {input}"),
     }
 }
 
-fn parse_packet(s: &str) -> IResult<&str, (Vec<Value>, Vec<Value>)> {
-    separated_pair(parse_row, tag("\n"), parse_row)(s)
+fn parse_packet(s: &str) -> IResult<&str, (Value, Value)> {
+    separated_pair(parse_value, tag("\n"), parse_value)(s)
 }
 
 fn parse_value(s: &str) -> IResult<&str, Value> {
@@ -57,10 +47,6 @@ fn parse_value(s: &str) -> IResult<&str, Value> {
             |v| Value::Array(v),
         ),
     ))(s)
-}
-
-fn parse_row(s: &str) -> IResult<&str, Vec<Value>> {
-    delimited(tag("["), separated_list0(tag(","), parse_value), tag("]"))(s)
 }
 
 #[cfg(test)]
@@ -91,22 +77,22 @@ mod tests {
     #[test]
     fn test_parse_row() {
         assert_eq!(
-            parse_row("[1,1,3,1,1]").unwrap().1,
-            vec![
+            parse_value("[1,1,3,1,1]").unwrap().1,
+            Value::Array(vec![
                 Value::Number(1),
                 Value::Number(1),
                 Value::Number(3),
                 Value::Number(1),
                 Value::Number(1)
-            ]
+            ])
         );
 
         assert_eq!(
-            parse_row("[[1],[2,3,4]]").unwrap().1,
-            vec![
+            parse_value("[[1],[2,3,4]]").unwrap().1,
+            Value::Array(vec![
                 Value::Array(vec![Value::Number(1)]),
                 Value::Array(vec![Value::Number(2), Value::Number(3), Value::Number(4)])
-            ]
+            ])
         );
     }
 
@@ -119,7 +105,10 @@ mod tests {
             )
             .unwrap()
             .1,
-            (vec![Value::Number(1)], vec![Value::Number(2)])
+            (
+                Value::Array(vec![Value::Number(1)]),
+                Value::Array(vec![Value::Number(2)])
+            )
         )
     }
 
