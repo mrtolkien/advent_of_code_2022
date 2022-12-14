acd90308acc58a8ec5f8debb42525f27b50ae9fe
mrtolkien <gary.mialaret@gmail.com>
2022-12-14 12:12:44 +0900

    !!!FINALLY SOME WORKING NOM PARSING!!!

--- src/day_13.rs
+++ src/day_13.rs
@@ -1,12 +1,22 @@
 use nom::multi::separated_list0;
+use nom::sequence::separated_pair;
 use nom::{
-    branch::alt, bytes::complete::tag, character::complete::digit1, combinator::map, multi::many0,
+    branch::alt, bytes::complete::tag, character::complete::digit1, combinator::map,
     sequence::delimited, IResult,
 };
-use nom::{error::ErrorKind, Err, Parser};
 
 pub fn get_right_order_pairs_index_sum(input: &str) -> usize {
-    todo!()
+    let input = parse_input(input);
+    let mut result = 0;
+
+    for (idx, (left, right)) in input.iter().enumerate() {
+        // TODO Get the right test here!
+        if left == right {
+            result += idx + 1;
+        }
+    }
+
+    result
 }
 
 #[derive(Debug, PartialEq)]
@@ -15,54 +25,36 @@ enum Value {
     Array(Vec<Value>),
 }
 
-fn parse_packet(s: &str) -> (Vec<Value>, Vec<Value>) {
-    todo!()
+fn parse_input(input: &str) -> Vec<(Vec<Value>, Vec<Value>)> {
+    match separated_list0(tag("\n\n"), parse_packet)(input) {
+        Ok((_, values)) => values,
+        Err(_) => panic!("Failed to parse input: {input}"),
+    }
 }
 
-fn parse_single_digit(s: &str) -> IResult<&str, Value> {
-    map(digit1, |s: &str| Value::Number(s.parse().unwrap()))(s)
-}
-
-fn parse_digit_list(s: &str) -> IResult<&str, Value> {
-    map(separated_list0(tag(","), parse_single_digit), |x| {
-        Value::Array(x)
-    })(s)
+fn parse_packet(s: &str) -> IResult<&str, (Vec<Value>, Vec<Value>)> {
+    separated_pair(parse_row, tag("\n"), parse_row)(s)
 }
 
 fn parse_value(s: &str) -> IResult<&str, Value> {
-    // TODO This should call itself with a separated_list somewhere
     alt((
-        parse_single_digit,
-        delimited(tag("["), parse_value, tag("]")),
-        parse_digit_list,
+        // If we have a single digit, we wrap it in a Value::Number and return
+        map(digit1, |s: &str| Value::Number(s.parse().unwrap())),
+        // If we have an array, we parse it as a list of values, wrap it in a Value::Array and return
+        map(
+            delimited(tag("["), separated_list0(tag(","), parse_value), tag("]")),
+            |v| Value::Array(v),
+        ),
     ))(s)
 }
 
 fn parse_row(s: &str) -> IResult<&str, Vec<Value>> {
-    delimited(tag("["), many0(parse_value), tag("]"))(s)
+    delimited(tag("["), separated_list0(tag(","), parse_value), tag("]"))(s)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn test_parse_digit_list() {
-        assert_eq!(
-            parse_digit_list("1,2").unwrap().1,
-            Value::Array(vec![Value::Number(1), Value::Number(2)])
-        );
-        assert_eq!(
-            parse_digit_list("1,2,3").unwrap().1,
-            Value::Array(vec![Value::Number(1), Value::Number(2), Value::Number(3)])
-        );
-    }
-
-    #[test]
-    fn test_parse_single_digit() {
-        assert_eq!(parse_single_digit("1").unwrap().1, Value::Number(1));
-        assert_eq!(parse_single_digit("123").unwrap().1, Value::Number(123));
-    }
 
     #[test]
     fn test_parse_value() {
@@ -82,6 +74,7 @@ mod tests {
                 Value::Array(vec![Value::Number(2), Value::Number(3), Value::Number(4)])
             ])
         );
+        assert_eq!(parse_value("[]").unwrap().1, Value::Array(vec![]));
     }
 
     #[test]
@@ -104,6 +97,19 @@ mod tests {
                 Value::Array(vec![Value::Number(2), Value::Number(3), Value::Number(4)])
             ]
         );
+    }
+
+    #[test]
+    fn test_parse_packet() {
+        assert_eq!(
+            parse_packet(
+                "[1]
+[2]"
+            )
+            .unwrap()
+            .1,
+            (vec![Value::Number(1)], vec![Value::Number(2)])
+        )
     }
 
     const DEMO_INPUT: &str = "[1,1,3,1,1]
