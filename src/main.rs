c1190b72779fcc416e3e2081b032ea656c3e3781
mrtolkien <gary.mialaret@gmail.com>
2022-12-15 15:03:44 +0900

    Day 14 structure

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use advent_of_code_2022::{
-    day_1, day_10, day_11, day_12, day_13, day_2, day_3, day_4, day_5, day_6, day_7, day_8, day_9,
+    day_1, day_10, day_11, day_12, day_13, day_14, day_2, day_3, day_4, day_5, day_6, day_7, day_8,
+    day_9,
 };
 use std::{fmt::Display, fs};
 
